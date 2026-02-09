4a1f96ba9bdc51857d3f34cef88942051b6ce1f1
Tom Puttemans <github@tomputtemans.com>
2026-02-09 23:27:18 +0100

    Allow bulk import of visible numbers in selected street

--- WME Quick HN Importer.user.js
+++ WME Quick HN Importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Quick HN Importer
 // @namespace    http://www.wazebelgium.be/
-// @version      2.0.5
+// @version      2.1.0
 // @description  Quickly add house numbers based on open data sources of house numbers
 // @author       Tom 'Glodenox' Puttemans
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
@@ -21,21 +21,110 @@
 
 let wmeSDK;
 const LAYER_NAME = 'Quick HN importer';
+const SHORTCUT_ID = 'quickhnimportercomplete';
 (unsafeWindow || window).SDK_INITIALIZED.then(() => {
   wmeSDK = getWmeSdk({ scriptId: "quick-hn-importer", scriptName: "Quick HN Importer"});
   wmeSDK.Events.once({ eventName: "wme-ready" }).then(init);
 });
 
-let loadingMessage = document.createElement('div');
-
 let previousCenterLocation = null;
 let selectedStreetNames = [];
+let autocompleteFeatures = [];
 let streetNumbers = new Map();
 let streetNames = new Set();
 
 proj4.defs("EPSG:3794","+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
 
-let repository = function() {
+let Messages = function() {
+  let lookup = new Map();
+  let container = document.createElement('div');
+  container.style.position = 'absolute';
+  container.style.bottom = '35px';
+  container.style.width = '100%';
+  container.style.pointerEvents = 'none';
+
+  return {
+    init: () => wmeSDK.Map.getMapViewportElement().appendChild(container),
+    add: (id, innerHTML, approximateSize) => {
+      let message = document.createElement('div');
+      message.style.margin = '5px auto';
+      message.style.width = approximateSize;
+      message.style.textAlign = 'center';
+      message.style.background = 'rgba(0, 0, 0, 0.5)';
+      message.style.color = 'white';
+      message.style.borderRadius = '3px';
+      message.style.padding = '5px 15px';
+      message.style.display = 'none';
+      message.innerHTML = innerHTML;
+      container.appendChild(message);
+      lookup.set(id, message);
+    },
+    show: (id, params) => {
+      let message = lookup.get(id);
+      params?.forEach((value, key) => message.querySelector(key).textContent = value);
+      message.style.display = null;
+    },
+    hide: (id) => {
+      lookup.get(id).style.display = 'none';
+    }
+  };
+}();
+Messages.add('loading', '<i class="fa fa-pulse fa-spinner"></i> Loading address points', '300px');
+Messages.add('autocomplete', '⭐ <span class="qhni-number"></span> missing house <span class="qhni-unit"></span> visible. Autofill with Ctrl+space', '500px');
+
+let Shortcut = function() {
+  const SHORTCUT_ID = "QuickHNImporterAutocomplete";
+  let added = false;
+  let callback = () => {
+    autocompleteFeatures.forEach(feature => {
+      // Try to find nearest segment with name match to latch to
+      let nearestSegment = findNearestSegment(feature, true);
+      if (!nearestSegment) {
+        nearestSegment = findNearestSegment(feature, false);
+      }
+      // Store house number
+      wmeSDK.DataModel.HouseNumbers.addHouseNumber({
+        number: feature.properties.number,
+        point: feature.geometry,
+        segmentId: nearestSegment.id
+      });
+      // Add to streetNumbers
+      let nameMatches = wmeSDK.DataModel.Streets.getAll().filter(street => street.name.toLowerCase() == feature.properties.street.toLowerCase()).length > 0;
+      if (nameMatches) {
+        if (!streetNumbers.has(feature.properties.street.toLowerCase())) {
+          streetNumbers.set(feature.properties.street.toLowerCase(), new Set());
+        }
+        streetNumbers.get(feature.properties.street.toLowerCase()).add(simplifyNumber(feature.properties.number));
+      }
+    });
+    wmeSDK.Map.redrawLayer({ layerName: LAYER_NAME });
+  };
+  return {
+    activate: () => {
+      if (added) {
+        return;
+      }
+      added = true;
+      wmeSDK.Shortcuts.createShortcut({
+        shortcutKeys: "C+32",
+        shortcutId: SHORTCUT_ID,
+        description: "",
+        callback: callback
+      });
+    },
+    deactivate: () => {
+      if (!added) {
+        return;
+      }
+      added = false;
+      wmeSDK.Shortcuts.deleteShortcut({
+        shortcutId: SHORTCUT_ID
+      });
+    }
+  };
+}();
+
+let Repository = function() {
   let groups = [];
   let directory = new Map();
   let toIndex = (lon, lat) => [ Math.floor(lon * 100), Math.floor(lat * 200) ];
@@ -122,12 +211,19 @@ let repository = function() {
         })
       });
     },
-    lookup: (featureId) => directory.get(featureId)
+    getFeatureById: (featureId) => directory.get(featureId),
+    getFeatures: (filterFunction) => {
+      groups.forEach((col, xIndex) => {
+        col.forEach((row, yIndex) => {
+          // Add to a collection to return if match is found
+        });
+      });
+    }
   };
 }();
 
 // Vlaanderen (Belgium):
-repository.addSource((left, bottom, right, top) => {
+Repository.addSource((left, bottom, right, top) => {
   let requestedPoly = turf.bboxPolygon([left, bottom, right, top]);
   let regionPoly = turf.polygon([[[4.777969,51.518210],[4.641333,51.422010],[4.537689,51.488676],[4.377120,51.453982],[4.382282,51.381726],[4.217576,51.373885],[3.965949,51.226031],[3.590019,51.305952],[3.414280,51.262159],[3.365430,51.370106],[3.186308,51.362487],[2.545706,51.088757],[2.574734,50.996934],[2.579035,50.918657],[2.606847,50.813426],[2.846267,50.697874],[2.963540,50.773141],[3.120955,50.770274],[3.418007,50.690563],[3.806240,50.747335],[3.920502,50.686118],[4.287230,50.688986],[4.798471,50.772137],[5.135715,50.690628],[5.508128,50.720813],[5.628842,50.773284],[5.821811,50.707623],[5.919873,50.709917],[5.907257,50.769270],[5.694503,50.814860],[5.653214,50.866185],[5.737226,50.906614],[5.866541,51.154922],[5.491785,51.305742],[5.343832,51.276209],[5.071179,51.393485],[5.136526,51.463444],[5.016099,51.491257],[5.002909,51.445380],[4.860834,51.471759],[4.777969,51.518210]]]);
   if (turf.booleanDisjoint(regionPoly, requestedPoly)) {
@@ -161,7 +257,7 @@ repository.addSource((left, bottom, right, top) => {
   });
 });
 // Brussels (Belgium):
-repository.addSource((left, bottom, right, top) => {
+Repository.addSource((left, bottom, right, top) => {
   let requestedPoly = turf.bboxPolygon([left, bottom, right, top]);
   let regionPoly = turf.polygon([[[4.410507,50.916487],[4.444648,50.883599],[4.420867,50.867712],[4.466045,50.851056],[4.476732,50.820404],[4.452249,50.806449],[4.485805,50.792925],[4.383965,50.761429],[4.331210,50.775508],[4.293584,50.804971],[4.238630,50.814280],[4.253596,50.836364],[4.279553,50.840647],[4.278150,50.866039],[4.295571,50.894146],[4.410507,50.916487]]]);
   if (turf.booleanDisjoint(regionPoly, requestedPoly)) {
@@ -189,7 +285,7 @@ repository.addSource((left, bottom, right, top) => {
   });
 });
 // Wallonie (Belgium):
-repository.addSource((left, bottom, right, top) => {
+Repository.addSource((left, bottom, right, top) => {
   let requestedPoly = turf.bboxPolygon([left, bottom, right, top]);
   let regionPoly = turf.polygon([[[5.709641,50.819673],[5.724607,50.758174],[6.025802,50.767641],[6.288645,50.632562],[6.197057,50.530405],[6.351289,50.488288],[6.420534,50.325417],[6.137793,50.129849],[5.999611,50.157914],[5.750798,49.830795],[5.921974,49.705737],[5.898589,49.553056],[5.472410,49.496955],[4.851679,49.793482],[4.781738,49.957938],[4.877586,50.153709],[4.702101,50.095553],[4.692983,49.995491],[4.454353,49.925431],[4.121356,49.959142],[4.147547,50.240543],[4.016592,50.344523],[3.673306,50.295549],[3.615312,50.482215],[3.286056,50.485191],[3.237416,50.688299],[3.055951,50.773557],[2.896000,50.685336],[2.794978,50.732724],[2.982056,50.818491],[3.175681,50.768824],[3.758521,50.780406],[4.249504,50.720289],[4.761160,50.831490],[5.137185,50.719105],[5.709641,50.819673]]]);
   if (turf.booleanDisjoint(regionPoly, requestedPoly)) {
@@ -217,7 +313,7 @@ repository.addSource((left, bottom, right, top) => {
   });
 });
 // The Netherlands:
-repository.addSource((left, bottom, right, top) => {
+Repository.addSource((left, bottom, right, top) => {
   let requestedPoly = turf.bboxPolygon([left, bottom, right, top]);
   let regionPoly = turf.polygon([[[4.276162,51.358135],[3.923522,51.188432],[3.644777,51.247607],[3.368838,51.233552],[3.263139,51.540013],[4.614772,53.286735],[6.399488,53.739490],[7.194590,53.245021],[7.192821,52.998006],[7.052388,52.600208],[6.737035,52.634695],[6.714814,52.461553],[7.071095,52.449966],[7.026320,52.230637],[6.725938,52.035871],[6.869987,51.957545],[6.362073,51.806845],[6.251697,51.852513],[5.946761,51.815520],[6.252633,51.468980],[6.147252,51.152253],[5.960792,51.034575],[6.085199,50.897317],[5.994910,50.749926],[5.681112,50.746043],[5.595992,50.835330],[5.802712,51.128007],[5.435105,51.254632],[5.129234,51.272191],[5.027527,51.476807],[4.910325,51.391902],[4.762117,51.413374],[4.826092,51.460555],[4.764184,51.496237],[4.630135,51.418206],[4.447338,51.433422],[4.401504,51.325998],[4.276162,51.358135]]]);
   if (turf.booleanDisjoint(regionPoly, requestedPoly)) {
@@ -256,7 +352,7 @@ repository.addSource((left, bottom, right, top) => {
   return retrieveData(0);
 });
 // Slovenia:
-repository.addSource((left, bottom, right, top) => {
+Repository.addSource((left, bottom, right, top) => {
   let requestedPoly = turf.bboxPolygon([left, bottom, right, top]);
   let regionPoly = turf.polygon([[[13.559654,45.463437],[13.568005,45.566997],[13.894686,45.631841],[13.546291,45.830907],[13.603082,45.954511],[13.442731,45.984577],[13.621456,46.168312],[13.410116,46.207981],[13.365897,46.300267],[13.731697,46.545804],[14.548483,46.418860],[14.850390,46.601135],[15.061953,46.649556],[15.458807,46.651034],[15.635975,46.717562],[15.934848,46.719517],[15.979947,46.843121],[16.278934,46.878198],[16.325338,46.839441],[16.526141,46.500705],[16.263947,46.515921],[16.285615,46.362069],[16.103550,46.370421],[16.048430,46.291915],[15.620828,46.174993],[15.697987,46.036209],[15.679289,45.820885],[15.286764,45.730688],[15.373769,45.640212],[15.268555,45.601662],[15.371951,45.455085],[15.144787,45.418338],[14.932656,45.506865],[14.820745,45.436712],[14.541802,45.627128],[14.423209,45.465107],[14.000618,45.471789],[13.889001,45.423636],[13.559654,45.463437]]]);
   if (turf.booleanDisjoint(regionPoly, requestedPoly)) {
@@ -284,13 +380,7 @@ repository.addSource((left, bottom, right, top) => {
 });
 
 function init() {
-  loadingMessage.style.position = 'absolute';
-  loadingMessage.style.bottom = '35px';
-  loadingMessage.style.width = '100%';
-  loadingMessage.style.pointerEvents = 'none';
-  loadingMessage.style.display = 'none';
-  loadingMessage.innerHTML = `<div style="margin:0 auto; max-width:300px; text-align:center; background:rgba(0, 0, 0, 0.5); color:white; border-radius:3px; padding:5px 15px;"><i class="fa fa-pulse fa-spinner"></i> Loading address points</div>`;
-  wmeSDK.Map.getMapViewportElement().appendChild(loadingMessage);
+  Messages.init();
 
   previousCenterLocation = Object.values(wmeSDK.Map.getMapCenter());
 
@@ -312,7 +402,6 @@ function init() {
       subtree: true,
     });
   }
-
   wmeSDK.Map.addLayer({
     layerName: LAYER_NAME,
     styleContext: {
@@ -359,7 +448,7 @@ function init() {
       // Check for any data removal when we're a good distance away
       if (turf.distance(currentLocation, previousCenterLocation) > 1) {
         previousCenterLocation = currentLocation;
-        repository.cull();
+        Repository.cull();
       }
     }
   });
@@ -367,7 +456,7 @@ function init() {
   wmeSDK.Events.on({
     eventName: "wme-layer-feature-clicked",
     eventHandler: (clickEvent) => {
-      let feature = repository.lookup(clickEvent.featureId);
+      let feature = Repository.getFeatureById(clickEvent.featureId);
       if (streetNumbers.has(feature.properties.street.toLowerCase()) && streetNumbers.get(feature.properties.street.toLowerCase()).has(simplifyNumber(feature.properties.number))) {
         return;
       }
@@ -499,8 +588,8 @@ function updateLayer() {
   } else if (wmeSDK.Map.isLayerVisible({ layerName: "house_numbers"}) && wmeSDK.Map.getZoomLevel() >= 19 && !wmeSDK.Map.isLayerVisible({ layerName: LAYER_NAME})) {
     wmeSDK.Map.setLayerVisibility({ layerName: LAYER_NAME, visibility: true });
   }
-  loadingMessage.style.display = null;
-  repository.getExtentData(wmeSDK.Map.getMapExtent()).then((features) => {
+  Messages.show('loading');
+  Repository.getExtentData(wmeSDK.Map.getMapExtent()).then((features) => {
     if (features.length > 0) {
       wmeSDK.Map.removeAllFeaturesFromLayer({
         layerName: LAYER_NAME
@@ -510,7 +599,27 @@ function updateLayer() {
         features: features
       });
     }
-    loadingMessage.style.display = 'none';
+    Messages.hide('loading');
+    Messages.hide('autocomplete');
+    // Pre-fill autocompleteFeatures
+    if (selectedStreetNames.length > 0) {
+      autocompleteFeatures = features.filter(feature => selectedStreetNames.includes(feature.properties.street.toLowerCase()) &&
+                                             (!streetNumbers.has(feature.properties.street.toLowerCase()) || (streetNumbers.has(feature.properties.street.toLowerCase()) && !streetNumbers.get(feature.properties.street.toLowerCase()).has(simplifyNumber(feature.properties.number)))) &&
+                                             turf.booleanContains(turf.bboxPolygon(wmeSDK.Map.getMapExtent()), feature));
+      if (autocompleteFeatures.length > 0) {
+        let params = new Map([
+          ['.qhni-number', autocompleteFeatures.length],
+          ['.qhni-unit', autocompleteFeatures.length > 1 ? 'numbers' : 'number']
+        ]);
+        Messages.show('autocomplete', params);
+        Shortcut.activate();
+      } else {
+        Shortcut.deactivate();
+      }
+    } else {
+      autocompleteFeatures = [];
+      Shortcut.deactivate();
+    }
   });
 }
 
@@ -535,14 +644,14 @@ function simplifyNumber(number) {
 function cleanupName(name) {
   const sanitizeChars = Object.entries({
     // EN DASH / HYPHEN (U+002D)
-    '\u1806': '\u002D', // '?'
-    '\u2010': '\u002D', // '-'
-    '\u2011': '\u002D', // '-'
-    '\u2012': '\u002D', // '-'
+    '\u1806': '\u002D', // '᠆'
+    '\u2010': '\u002D', // '‐'
+    '\u2011': '\u002D', // '‑'
+    '\u2012': '\u002D', // '‒'
     '\u2013': '\u002D', // '–'
-    '\uFE58': '\u002D', // '?'
-    '\uFE63': '\u002D', // '?'
-    '\uFF0D': '\u002D', // '-'
+    '\uFE58': '\u002D', // '﹘'
+    '\uFE63': '\u002D', // '﹣'
+    '\uFF0D': '\u002D', // '－'
 
     // SINGLE QUOTES (U+0027)
     '\u003C': '\u0027', // '<'
@@ -550,16 +659,16 @@ function cleanupName(name) {
     '\u2018': '\u0027', // '‘'
     '\u2019': '\u0027', // '’'
     '\u201A': '\u0027', // '‚'
-    '\u201B': '\u0027', // '''
+    '\u201B': '\u0027', // '‛'
     '\u2039': '\u0027', // '‹'
     '\u203A': '\u0027', // '›'
-    '\u275B': '\u0027', // '?'
-    '\u275C': '\u0027', // '?'
-    '\u276E': '\u0027', // '?'
-    '\u276F': '\u0027', // '?'
-    '\uFF07': '\u0027', // '''
-    '\u300C': '\u0027', // '?'
-    '\u300D': '\u0027', // '?'
+    '\u275B': '\u0027', // '❛'
+    '\u275C': '\u0027', // '❜'
+    '\u276E': '\u0027', // '❮'
+    '\u276F': '\u0027', // '❯'
+    '\uFF07': '\u0027', // '＇'
+    '\u300C': '\u0027', // '「'
+    '\u300D': '\u0027', // '」'
 
     // // DOUBLE QUOTES (U+0022)
     '\u00AB': '\u0022', // '«'
@@ -567,16 +676,16 @@ function cleanupName(name) {
     '\u201C': '\u0022', // '“'
     '\u201D': '\u0022', // '”'
     '\u201E': '\u0022', // '„'
-    '\u201F': '\u0022', // '"'
-    '\u275D': '\u0022', // '?'
-    '\u275E': '\u0022', // '?'
-    '\u2E42': '\u0022', // '?'
-    '\u301D': '\u0022', // '?'
-    '\u301E': '\u0022', // '?'
-    '\u301F': '\u0022', // '?'
-    '\uFF02': '\u0022', // '"'
-    '\u300E': '\u0022', // '?'
-    '\u300F': '\u0022', // '?'
+    '\u201F': '\u0022', // '‟'
+    '\u275D': '\u0022', // '❝'
+    '\u275E': '\u0022', // '❞'
+    '\u2E42': '\u0022', // '⹂'
+    '\u301D': '\u0022', // '〝'
+    '\u301E': '\u0022', // '〞'
+    '\u301F': '\u0022', // '〟'
+    '\uFF02': '\u0022', // '＂'
+    '\u300E': '\u0022', // '『'
+    '\u300F': '\u0022', // '』'
   });
 
   return sanitizeChars.reduce((acc, [char, stdChar]) => {
